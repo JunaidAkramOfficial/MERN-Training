d4a6f81b0ac53746a28ff3fd561b4ce596ddfe63
Junaid Akram <junaid.akram@invozone.com>
2022-12-08 01:34:50 +0500

    Day-5

---
+++ Day 5 - Node Js Syntax/Node Ch/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+// CURD Operation FIle System
+
+// fs.mkdirSync("Junaid");
+
+// fs.writeFileSync(
+//   "Junaid/bio.txt",
+//   "my name is Junaid Akram. My Age is 23 Year Old"
+// );
+
+// fs.appendFileSync(
+//   "Junaid/bio.txt",
+//   "my name is Junaid Akram. My Age is 23 Year Old. I am a MERN Stack Developer"
+// );
+
+// const data = fs.readFileSync("Junaid/bio.txt" , 'utf-8');
+// console.log(data);
+
+// fs.renameSync("Junaid/bio.txt", "Junaid/MyBio.txt");
+
+// fs.writeFileSync("Junaid/bio.txt", "New File");
+
+// fs.unlinkSync("Junaid/bio.txt");
+
+// fs.rmdirSync("Junaid");
---
+++ Day 5 - Node Js Syntax/fsAsync/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+// CURD Operation FIle System
+
+fs.mkdirSync("Junaid");
+
+// fs.appendFile(
+//   "read.txt",
+//   "Node Js Practicing with file system async",
+//   (err) => {
+//     console.log("task completed");
+//   }
+// );
+
+// // Sync
+
+// const data = fs.readFileSync("read.txt", "UTF-8");
+// console.log(data);
+// console.log("after the data");
+
+// // Async
+
+// fs.readFile("read.txt", "UTF-8", (err, data) => {
+//   console.log(data);
+// });
+// console.log("after the data");
